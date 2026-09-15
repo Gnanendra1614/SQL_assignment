648fb91759cbebc00bd96fbc4277f2c04ab1a8f9
gnanendra reddy <avula.g@diggibyte.com>
2026-09-15 16:07:46 +0530

    Added question-3 total salary of each department

---
+++ src/question_03/01_question.sql
@@ -0,0 +1,30 @@
+-- Total salary of each department
+
+CREATE TABLE dept_tbl
+(
+    id_deptname VARCHAR(30),
+    emp_name VARCHAR(50),
+    salary INT
+);
+
+INSERT INTO dept_tbl
+VALUES
+('1111-MATH', 'RAHUL', 10000),
+('1111-MATH', 'RAKESH', 20000),
+('2222-SCIENCE', 'AKASH', 10000),
+('222-SCIENCE', 'ANDREW', 10000),
+('22-CHEM', 'ANKIT', 25000),
+('3333-CHEM', 'SONIKA', 12000),
+('4444-BIO', 'HITESH', 2300),
+('44-BIO', 'AKSHAY', 10000);
+
+SELECT
+    SUBSTRING(id_deptname,
+        CHARINDEX('-', id_deptname) + 1,
+        LEN(id_deptname)) AS Department,
+    SUM(salary) AS Total_Salary
+FROM dept_tbl
+GROUP BY
+    SUBSTRING(id_deptname,
+        CHARINDEX('-', id_deptname) + 1,
+        LEN(id_deptname));
